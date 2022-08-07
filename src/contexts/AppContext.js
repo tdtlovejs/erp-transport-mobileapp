f429a997a09741872c9d5bc981f633039acf3a66
trandinhthang <trandinhthang99.hy@gmail.com>
2022-08-07 16:52:19 +0700

    update

--- src/contexts/AppContext.js
+++ src/contexts/AppContext.js
@@ -36,54 +36,54 @@ const AppContextProvider = ({children}) => {
     const [currentLocation, setCurrentLocation] = useState(null);
 
     useEffect(() => {
-        const onLocation = BackgroundGeolocation.onLocation((location) => {
-            setLocation(JSON.stringify(location));
-        })
-        const onMotionChange = BackgroundGeolocation.onMotionChange((event) => {
-            // console.log('[onMotionChange]', event);
-        });
-        const onActivityChange = BackgroundGeolocation.onActivityChange((event) => {
-            // console.log('[onMotionChange]', event);
-        })
-        const onProviderChange = BackgroundGeolocation.onProviderChange((event) => {
-            // console.log('[onProviderChange]', event);
-        })
-        // if (currentGpsRoute) {
-        //     getLocation();
+        // const onLocation = BackgroundGeolocation.onLocation((location) => {
+        //     setLocation(JSON.stringify(location));
+        // })
+        // const onMotionChange = BackgroundGeolocation.onMotionChange((event) => {
+        //     // console.log('[onMotionChange]', event);
+        // });
+        // const onActivityChange = BackgroundGeolocation.onActivityChange((event) => {
+        //     // console.log('[onMotionChange]', event);
+        // })
+        // const onProviderChange = BackgroundGeolocation.onProviderChange((event) => {
+        //     // console.log('[onProviderChange]', event);
+        // })
+        // // if (currentGpsRoute) {
+        // //     getLocation();
+        // // }
+        // BackgroundGeolocation.ready({
+        //     // Geolocation Config
+        //     desiredAccuracy: BackgroundGeolocation.DESIRED_ACCURACY_HIGH,
+        //     distanceFilter: 10,
+        //     // // Activity Recognition
+        //     // stopTimeout: 5,
+        //     // // Application config
+        //     // debug: true, // <-- enable this hear sounds for background-geolocation life-cycle.
+        //     // logLevel: BackgroundGeolocation.LOG_LEVEL_VERBOSE,
+        //     // stopOnTerminate: false,   // <-- Allow the background-service to continue tracking when user closes the app.
+        //     // startOnBoot: true,        // <-- Auto start tracking when device is powered-up.
+        //     // // HTTP / SQLite config
+        //     // url: 'http://yourserver.com/locations',
+        //     // batchSync: false,       // <-- [Default: false] Set true to sync locations to server in a single HTTP request.
+        //     // autoSync: true,         // <-- [Default: true] Set true to sync each location to server as it arrives.
+        //     // headers: {              // <-- Optional HTTP headers
+        //     //     "X-FOO": "bar"
+        //     // },
+        //     // params: {               // <-- Optional HTTP params
+        //     //     "auth_token": "maybe_your_server_authenticates_via_token_YES?"
+        //     // }
+        // }).then((state) => {
+        //     // console.log(state)
+        //     // setEnabled(state)
+        //     // console.log("- BackgroundGeolocation is configured and ready: ", state.enabled);
+        // });
+        // return () => {
+        //     onLocation.remove();
+        //     onMotionChange.remove();
+        //     onActivityChange.remove();
+        //     onProviderChange.remove();
+        //     // watchPosition.remove()
         // }
-        BackgroundGeolocation.ready({
-            // Geolocation Config
-            desiredAccuracy: BackgroundGeolocation.DESIRED_ACCURACY_HIGH,
-            distanceFilter: 10,
-            // // Activity Recognition
-            // stopTimeout: 5,
-            // // Application config
-            // debug: true, // <-- enable this hear sounds for background-geolocation life-cycle.
-            // logLevel: BackgroundGeolocation.LOG_LEVEL_VERBOSE,
-            // stopOnTerminate: false,   // <-- Allow the background-service to continue tracking when user closes the app.
-            // startOnBoot: true,        // <-- Auto start tracking when device is powered-up.
-            // // HTTP / SQLite config
-            // url: 'http://yourserver.com/locations',
-            // batchSync: false,       // <-- [Default: false] Set true to sync locations to server in a single HTTP request.
-            // autoSync: true,         // <-- [Default: true] Set true to sync each location to server as it arrives.
-            // headers: {              // <-- Optional HTTP headers
-            //     "X-FOO": "bar"
-            // },
-            // params: {               // <-- Optional HTTP params
-            //     "auth_token": "maybe_your_server_authenticates_via_token_YES?"
-            // }
-        }).then((state) => {
-            // console.log(state)
-            // setEnabled(state)
-            // console.log("- BackgroundGeolocation is configured and ready: ", state.enabled);
-        });
-        return () => {
-            onLocation.remove();
-            onMotionChange.remove();
-            onActivityChange.remove();
-            onProviderChange.remove();
-            // watchPosition.remove()
-        }
     }, [])
 
     const getLocation = () => {
@@ -100,12 +100,12 @@ const AppContextProvider = ({children}) => {
         })
     }
     useEffect(() => {
-        if (enabled) {
-            BackgroundGeolocation.start();
-        } else {
-            BackgroundGeolocation.stop();
-            setLocation('');
-        }
+        // if (enabled) {
+        //     BackgroundGeolocation.start();
+        // } else {
+        //     BackgroundGeolocation.stop();
+        //     setLocation('');
+        // }
     }, [enabled]);
 
     useEffect(() => {
@@ -187,14 +187,14 @@ const AppContextProvider = ({children}) => {
         await AsyncStorage.setItem(KEY_STORAGE_LANGUAGE, language);
     }
 
-    useEffect(() => {
-        if (currentGpsRoute) {
-            getLocation();
-        } else {
-            BackgroundGeolocation.stopWatchPosition((success) => {
-            })
-        }
-    }, currentGpsRoute)
+    // useEffect(() => {
+    //     if (currentGpsRoute) {
+    //         getLocation();
+    //     } else {
+    //         BackgroundGeolocation.stopWatchPosition((success) => {
+    //         })
+    //     }
+    // }, currentGpsRoute)
     const saveCurrentGpsRouteToStorage = async (currentGpsRoute) => {
         if (currentGpsRoute) {
             await AsyncStorage.setItem(KEY_STORAGE_GPS_ROUTE, JSON.stringify(currentGpsRoute));
